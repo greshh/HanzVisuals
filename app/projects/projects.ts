f57a409265aac84353902ff040f5bd18b2eff0d2
greshh <greshkalao@gmail.com>
2026-05-20 11:01:47 +1200

    v1.3.2

--- app/projects/projects.ts
+++ app/projects/projects.ts
@@ -188,6 +188,6 @@ export const projects: Project[] = [
     "startDate": "2026-05-16",
     "endDate": "2026-05-17",
     "tags": ["photos", "basketball"],
-    "link": "https://photos.google.com/u/4/album/AF1QipP4hh9wDxnywf91Y99sHSGG6GVCsKfMwTfXzcEv"
+    "link": "https://photos.app.goo.gl/rLNWaMtkuKs7pvYu7"
   },
 ]
